9a22ba9110d163702118f8745022c1e78cc7a4aa
Oscar Perez-Hernandez <perezoscar360@gmail.com>
2023-07-26 16:41:03 -0500

    fixed my server port and seed.js file. my db seeds correctly will not work on the landing page

--- Server/seed.js
+++ Server/seed.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
-const {CONNECTION_STRING} = process.env
+require('dotenv').config()
+const { CONNECTION_STRING } = process.env;
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(CONNECTION_STRING, {
+
+const sequelize = new Sequelize(CONNECTION_STRING,{
     dialect:'postgres',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
+    dialectOptions : {
+       ssl:{
+        rejectUnauthorized: false
+       } 
     }
 })
 
-module.exports= {
+module.exports = {
     seed: (req, res) => {
         sequelize.query(`
 
-            DROP TABLE IF EXISTS user;
+            DROP TABLE IF EXISTS users;
             DROP TABLE IF EXISTS employee;
 
-            CREATE TABLE user(
-                user_id SERIAL PRIMARY KEY,
+            CREATE TABLE users (
+                users_id SERIAL PRIMARY KEY,
                 first_name VARCHAR(35) NOT NULL,
                 last_name VARCHAR(35) NOT NULL,
                 user_name VARCHAR(35),
@@ -29,10 +30,10 @@ module.exports= {
 
             CREATE TABLE employee (
                 employee_id SERIAL PRIMARY KEY,
+                users_id INTEGER REFERENCES users(users_id),
                 first_name VARCHAR(35) NOT NULL,
                 last_name VARCHAR(35) NOT NULL,
-                hourly_wage FLOAT(15) NOT NULL,
-                user_id REFERENCES user(user_id)
+                hourly_wage FLOAT NOT NULL
             );
 
             CREATE TABLE subscribers (
@@ -41,14 +42,22 @@ module.exports= {
                 email VARCHAR(100)
             );
 
-            INSERT INTO user(first_name,last_name,user_name,email,password) VALUES('Oscar','Perez-Hernandez','OPHManager','perezoscar1234@gmail.com','Password')
+            INSERT INTO users(first_name,last_name,user_name,email,password) VALUES('Oscar','Perez-Hernandez','OPHManager','perezoscar1234@gmail.com','Password');
 
-            INSERT INTO employee(first_name,last_name,hourly_wage) VALUES('Alex','Hernandez',30.25), ('Carmen','Hernandez',40.50), ('Daisy','Martinez',25.25), ('Edwin','Facio',20.50), ('Daniela','Uresti',20.50), ('Leonardo','Garcia',24.75), ('Leon','Perez',25.25), ('Roman','Hernandez',35.25), ('Leslie','Maldonado',25.25), ('Pablo','Villa',25.25);
+            INSERT INTO employee(users_id,first_name,last_name,hourly_wage) VALUES(1,'Alex','Hernandez',30.25), 
+            (1, 'Carmen','Hernandez',40.50), 
+            (1, 'Daisy','Martinez',25.25),
+            (1, 'Edwin','Facio',20.50), 
+            (1, 'Daniela','Uresti',20.50), 
+            (1, 'Leonardo','Garcia',24.75), 
+            (1, 'Leon','Perez',25.25), 
+            (1, 'Roman','Hernandez',35.25), 
+            (1, 'Leslie','Maldonado',25.25), 
+            (1, 'Pablo','Villa',25.25);
 
-        `)
-        .then(() => {
-            console.log('DB seeded successfully!')
-            res.sendStatus(200)
-        }).catch(err => console.log('error seeding DB', err))
+            `).then(() => {
+                console.log('DB seeded!')
+                res.sendStatus(200)
+            }).catch(err => console.log('error seeding DB', err))
+        }
     }
-}
--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
 const {SERVER_PORT} = process.env
+const {seed} = require('./seed.js')
+const bcrypt = require('bcrypt');
 
-const app = express();
+
 const {
     login,
     register
 } = require('./controllers/auth')
 
-app.post(`/api/login`, login)
-app.post(`/api/register`, register)
-
-const {seed, } = require('./controllers')
 
 app.use(express.json());
 app.use(cors());
+
+// TO SEED DB UN COMMENT, MAKE REQUEST THEN COMMENT POST REQUEST
+app.post('/seed', seed)
+app.post(`/api/login`, login)
+app.post(`/api/register`, register)
 
 app.listen(SERVER_PORT, () => console.log(`Server is up and listening on ${SERVER_PORT}`))
