03c6937348ebbf2cf4ee0893084c17171cfa9473
Oscar Perez-Hernandez <perezoscar360@gmail.com>
2023-08-09 00:31:58 -0500

    added img and style to landingpage, looks good now and edited employee portal to member portal

--- Client/landingPage.js
+++ Client/landingPage.js
@@ -2,6 +2,8 @@ const ePortalBtn = document.getElementById('employeePortal');
 const pPortalBtn = document.getElementById('payrollPortal');
 const mPortalBtn = document.getElementById('ManagerPortal');
 const subBtn = document.getElementById('subBtn')
+// variable from price page
+const priceBtn = document.getElementById('priceBtn');
 
 
 ePortalBtn.addEventListener('click', () => {
@@ -26,4 +28,12 @@ const getNotification = () => {
     window.alert("You've been subscribed to our newsletter!")
 }
 
+
 subBtn.addEventListener('click', getNotification);
+
+priceBtn.addEventListener('click', () => {
+    console.log(priceBtn.textContent);
+
+    window.location.href = './splash.html'
+
+});
